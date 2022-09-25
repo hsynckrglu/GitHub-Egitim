0d6f131726af0a90c67e19fd0f681e87d787c148
hsynckrglu <huseyincakirogluu@outlook.com>
2022-09-25 15:29:39 +0300

    yorum satırı eklendi

--- Giris/src/SeleniumEdgeDriver.java
+++ Giris/src/SeleniumEdgeDriver.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class SeleniumEdgeDriver {
-
+//denemeBranchine aittir
 	public static void main(String[] args) {
 		
 		//microsoft edge --> edge driver
